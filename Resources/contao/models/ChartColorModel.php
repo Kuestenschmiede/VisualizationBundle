1fe874e44bf348e1331285bf5186baba211fa061
rro <raphael.ross@kuestenschmiede.gmbh>
2019-08-14 10:19:10 +0200

    Rename ColorSetModel and its table

---
+++ Resources/contao/models/ChartColorModel.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace con4gis\VisualizationBundle\Resources\contao\models;
+
+use Contao\Model;
+
+class ChartColorModel extends Model
+{
+    protected static $strTable = 'tl_c4g_visualization_chart_color';
+}
