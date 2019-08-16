0c1fb4121ce430bec0ed893ea0bec130d3c9ab55
rro <raphael.ross@kuestenschmiede.gmbh>
2019-08-16 14:49:05 +0200

    Add backend modules and models to config.php

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -16,7 +16,26 @@
 $GLOBALS['con4gis']['visualization']['installed'] = true;
 
 /**
+ * Backend modules
+ */
+array_insert($GLOBALS['BE_MOD'], array_search('con4gis_core', array_keys($GLOBALS['BE_MOD'])) + 2,
+    ['con4gis_visualization' => [
+        'tl_c4g_visualization_chart' => array
+        (
+            'tables' 		=> array('tl_c4g_visualization_chart')
+        ),
+        'tl_c4g_visualization_chart_element' => array
+        (
+            'tables' 		=> array('tl_c4g_visualization_chart_element')
+        ),
+
+    ]]
+);
+
+/**
  * MODELS
  */
-//$GLOBALS['TL_MODELS']['tl_c4g_projects'] = 'con4gis\ProjectsBundle\Classes\Models\C4gProjectsModel';
-//$GLOBALS['TL_MODELS']['tl_c4g_projects_logbook'] = 'con4gis\ProjectsBundle\Classes\Models\C4gProjectsLogbookModel';
+$GLOBALS['TL_MODELS']['tl_c4g_visualization_chart_element_input'] = \con4gis\VisualizationBundle\Resources\contao\models\ChartElementModel::class;
+$GLOBALS['TL_MODELS']['tl_c4g_visualization_chart_element'] = \con4gis\VisualizationBundle\Resources\contao\models\ChartElementModel::class;
+$GLOBALS['TL_MODELS']['tl_c4g_visualization_chart_element_relation'] = \con4gis\VisualizationBundle\Resources\contao\models\ChartElementRelationModel::class;
+$GLOBALS['TL_MODELS']['tl_c4g_visualization_chart'] = \con4gis\VisualizationBundle\Resources\contao\models\ChartModel::class;
